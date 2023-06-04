0f7624b86b52a6409361a2292c28bc8454413f7c
Tabhi109 <tabhinav109@gmail.com>
2023-06-04 15:36:33 +0530

    Created a REST API with db.json for fetching

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import { useEffect, useState } from "react";
 function App() {
   const [solution,setsolution] = useState(null);
   useEffect(() => {
-    fetch("http://localhost:5000/solution")
+    fetch("http://localhost:3000/solutions")
       .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setsolution(data);
+      .then(json => {
+        const randomSolution = json[Math.floor(Math.random() * json.length)];
+        setsolution(randomSolution.word);
       });
-  },[]);
+  },[setsolution]);
   return (
     <div className="App">
       <h1>Wordle</h1>
+      {solution && <div>Solution is : {solution}</div>}
     </div>
   );
 }
