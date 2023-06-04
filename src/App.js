187e13c6c1020ed7372e35a8bb3dfaa67df7f411
Tabhi109 <tabhinav109@gmail.com>
2023-06-04 15:23:29 +0530

    Used hooks for fetching the words from the API and displaying them on the screen.!

--- src/App.js
+++ src/App.js
@@ -1,4 +1,15 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [solution,setsolution] = useState(null);
+  useEffect(() => {
+    fetch("http://localhost:5000/solution")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setsolution(data);
+      });
+  },[]);
   return (
     <div className="App">
       <h1>Wordle</h1>
