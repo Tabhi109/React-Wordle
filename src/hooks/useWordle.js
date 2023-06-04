a246ec400e0b4542c09c46a5af4d745ab9e10f80
Tabhi109 <tabhinav109@gmail.com>
2023-06-04 15:42:02 +0530

    added a component , and initiated necessary hooks!

---
+++ src/hooks/useWordle.js
@@ -0,0 +1,24 @@
+const UseWordle = (solution) => {
+
+    const [turn,setTurn] = useState(0);
+    const [currentGuess,setCurrentGuess] = useState("");
+    const [guesses,setGuesses] = useState([]);
+    const [history,setHistory] = useState([]);
+    const [isCorrect,setIsCorrect] = useState(false);
+
+    
+    const formatGuess = () => {
+
+    }
+
+    const addNewGuess = () => {
+    
+    }
+
+    const handleKeyup = () => {
+
+    }
+
+}
+
+export default UseWordle;
