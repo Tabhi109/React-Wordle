68ad9ea3a8adea6a44a8f137120ee505e06eb026
Tabhi109 <tabhinav109@gmail.com>
2023-06-05 23:35:06 +0530

    created grid layout !

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import { useEffect, useState } from "react";
-import Wordle from "./components/Wordle";
+import { useEffect, useState } from 'react'
+import Wordle from './components/Wordle'
+
 function App() {
-  const [solution,setsolution] = useState(null);
+  const [solution, setSolution] = useState(null)
+  
   useEffect(() => {
-    fetch("http://localhost:3000/solutions")
-      .then((res) => res.json())
+    fetch('http://localhost:3000/solutions')
+      .then(res => res.json())
       .then(json => {
-        const randomSolution = json[Math.floor(Math.random() * json.length)];
-        setsolution(randomSolution.word);
-      });
-  },[setsolution]);
+        // random int between 0 & 14
+        const randomSolution = json[Math.floor(Math.random()*json.length)]
+        setSolution(randomSolution.word)
+      })
+  }, [setSolution])
+
   return (
     <div className="App">
       <h1>Wordle</h1>
-      {solution && <div>Solution is : {solution}</div>}
-      <Wordle solution={solution} />
+      {solution && <Wordle solution={solution} />}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -1,19 +1,25 @@
 import React, { useEffect } from 'react'
-import useWordle from '../hooks/useWordle';
-const Wordle = ({ solution }) => {
-    const {currentGuess,handleKeyup} = useWordle(solution);
-    useEffect(() => {
-        window.addEventListener("keyup",handleKeyup);
-        return () => {
-            window.removeEventListener("keyup",handleKeyup);
-        }
-    },[handleKeyup]);
+import useWordle from '../hooks/useWordle'
+import Grid from './Grid'
+
+export default function Wordle({ solution }) {
+  const { currentGuess, guesses, turn, isCorrect, handleKeyup } = useWordle(solution)
+  
+  useEffect(() => {
+    window.addEventListener('keyup', handleKeyup)
+
+    return () => window.removeEventListener('keyup', handleKeyup)
+  }, [handleKeyup])
+
+  useEffect(() => {
+    console.log(guesses, turn, isCorrect)
+  }, [guesses, turn, isCorrect])
+
   return (
     <div>
-        <h1>current guess - {currentGuess}</h1>
-        
+      <div>solution - {solution}</div>
+      <div>Current Guess - {currentGuess}</div>
+      <Grid guesses={guesses} currentGuess={currentGuess} turn={turn} />
     </div>
   )
 }
-
-export default Wordle
