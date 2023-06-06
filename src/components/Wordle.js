c9ed1fd6a32d640f238c94c6c1741ffe8e98f3e0
Tabhi109 <tabhinav109@gmail.com>
2023-06-06 20:38:15 +0530

    minor updations !

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -18,7 +18,6 @@ export default function Wordle({ solution }) {
   return (
     <div>
       <div>solution - {solution}</div>
-      <div>Current Guess - {currentGuess}</div>
       <Grid guesses={guesses} currentGuess={currentGuess} turn={turn} />
     </div>
   )
