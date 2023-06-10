c02d284f7b6bba5aec5602a08e3a6ef69a980b87
Tabhi109 <tabhinav109@gmail.com>
2023-06-11 00:03:35 +0530

    finalized data structures

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,17 +1,15 @@
 import React from 'react'
 import { useEffect,useState } from 'react';
+import letters from '../Data/letters';
 export default function Keypad({usedKeys}) {
-    const [letters,setLetters] = useState(null)
+  const [letter,setLetter] = useState(null)
 
     useEffect(() => {
-      fetch('http://localhost:3000/letters')
-        .then(res => res.json())
-        .then(json => {
-          setLetters(json)})
+          setLetter(letters)
     }, []);
   return (
     <div className='keypad'>
-      {letters && letters.map((l) => {
+      {letter && letter.map((l) => {
         const color = usedKeys[l.key]
         return (
           <div key={l.key} className={color}>{l.key}</div>
