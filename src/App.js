054ca85e611eb7e6a08ccca8f090ea84b1ae99ad
Tabhi109 <tabhinav109@gmail.com>
2023-06-10 23:57:56 +0530

    changed data structures

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import { useEffect, useState } from 'react'
-import Wordle from './components/Wordle'
+import { useEffect, useState } from 'react';
+import Wordle from './components/Wordle';
+import solutions from './Data/solutions';
 
 function App() {
-  const [solution, setSolution] = useState(null)
+  const [solution, setSolution] = useState(null);
   
   useEffect(() => {
-    fetch('http://localhost:3000/solutions')
-      .then(res => res.json())
-      .then(json => {
-        // random int between 0 & 14
-        const randomSolution = json[Math.floor(Math.random()*json.length)]
-        setSolution(randomSolution.word)
-      })
-  }, [setSolution])
+    const randomSolution = solutions[Math.floor(Math.random() * solutions.length)];
+    setSolution(randomSolution);
+  }, []);
+
+  console.log(solution);
 
   return (
     <div className="App">
@@ -22,7 +20,7 @@ function App() {
       
       {solution && <Wordle solution={solution} />}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
